b9ec0947e13dd6fcc1e89868069a8f9dd8ae4736
Manny Gimond <mgimond@colby.edu>
2023-02-06 12:57:27 -0500

    added 2022 data

---
+++ data_extraction.R
@@ -0,0 +1,15 @@
+library(dataRetrieval)
+library(lubridate)
+
+# Choptank River near Greensboro, MD
+siteNumber <- "01049500" # Gardiner 
+
+# Params 
+parameterCd <- "00060" 
+
+# Raw daily data:
+dat <- readNWISdv(siteNumber,parameterCd, "1976-01-01","2023-01-01")
+
+# Prep for output
+dat$Discharge <- dat$X_00060_00003
+saveRDS(dat[ , c("Date", "Discharge")], file = "Kennebec.Rds")
